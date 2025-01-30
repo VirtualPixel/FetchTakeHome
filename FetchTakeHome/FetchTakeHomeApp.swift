86621971b456884378b88adad5ffd5d217dba45e
Justin Wells <justin.wells.ext@bayer.com>
2025-01-30 16:37:59 -0500

    initial commit

---
+++ FetchTakeHome/FetchTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeApp.swift
+//  FetchTakeHome
+//
+//  Created by Justin Wells on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
